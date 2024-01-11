9e4b6d963a75437babd531ec11f4b63fd8a5e815
Aya <ayaelrhayour123@gmail.com>
2024-01-11 16:32:51 +0100

    added another define to config

--- app/config/config.php
+++ app/config/config.php
@@ -5,9 +5,9 @@
   define('DB_PASS', '');
   define('DB_NAME', 'wiki');
 
-
+  define('SITENAME', 'wiki');
   define('APPROOT', dirname(dirname(__FILE__)));
   define('URLROOT', 'http://localhost/wiki');
-  define('SITENAME', 'wiki');
+  define("ABSOLUTE", "C:/xampp/htdocs/wiki/public/img/");
 
 ?>
