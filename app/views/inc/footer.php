94ec8ea6363e3a920d3e3352863ddb1251d7d4d9
Aya <ayaelrhayour123@gmail.com>
2024-01-11 16:30:00 +0100

    changed the footer to an included version

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -1,3 +1,50 @@
-  <script src="<?php echo URLROOT; ?>/js/main.js"></script>
+<footer class="site-footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4">
+          <div class="widget">
+            <h3 class="mb-4">About this project</h3>
+            <p>Wiki is an efficient content management system, combined with a front office to deliver an exceptional user experience.</p>
+          </div> 
+          <div class="widget">
+            <h3>Social</h3>
+            <ul class="list-unstyled social">
+              <li><a href="#"><ion-icon name="logo-instagram"></ion-icon></a></li>
+              <li><a href="#"><ion-icon name="logo-twitter"></ion-icon></a></li>
+              <li><a href="#"><ion-icon name="logo-facebook"></ion-icon></a></li>
+              <li><a href="#"><ion-icon name="logo-linkedin"></ion-icon></a></li>
+              <li><a href="#"><ion-icon name="logo-pinterest"></ion-icon></a></li>
+            </ul>
+          </div>
+        </div>
+
+
+      <div class="row mt-5">
+        <div class="col-12 text-center">
+
+            <p>Copyright &copy;<script>document.write(new Date().getFullYear());</script>. All Rights Reserved.</p>
+          </div>
+        </div>
+      </div> <!-- /.container -->
+  </footer> <!-- /.site-footer -->
+
+ 
+
+
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/tiny-slider.js"></script>
+
+    <script src="js/flatpickr.min.js"></script>
+
+
+    <script src="js/aos.js"></script>
+    <script src="js/glightbox.min.js"></script>
+    <script src="js/navbar.js"></script>
+    <script src="js/counter.js"></script>
+    <script src="js/custom.js"></script>
+
+      <!-- ====== ionicons ======= -->
+  <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </body>
 </html>
