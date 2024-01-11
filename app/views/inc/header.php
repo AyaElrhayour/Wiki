b0081fb56912eff1d4cd932b8213f0eb0ae794d5
Aya <ayaelrhayour123@gmail.com>
2024-01-11 16:29:38 +0100

    changed the header to included version

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -1,11 +1,82 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">
-  <title><?php echo SITENAME; ?></title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="author" content="Untree.co">
+	<link rel="shortcut icon" href="favicon.png">
+
+	<meta name="description" content="" />
+	<meta name="keywords" content="bootstrap, bootstrap5" />
+
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+
+
+	<link rel="stylesheet" href="fonts/icomoon/style.css">
+	<link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+
+	<link rel="stylesheet" href="<?php echo URLROOT; ?>/css/tiny-slider.css">
+	<link rel="stylesheet" href="<?php echo URLROOT; ?>/css/aos.css">
+	<link rel="stylesheet" href="<?php echo URLROOT; ?>/css/glightbox.min.css">
+	<link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">
+
+	<link rel="stylesheet" href="css/flatpickr.min.css">
+	<title>Wiki</title>
 </head>
 <body>
   
+<div class="site-mobile-menu site-navbar-target">
+		<div class="site-mobile-menu-header">
+			<div class="site-mobile-menu-close">
+				<span class="icofont-close js-menu-toggle"></span>
+			</div>
+		</div>
+		<div class="site-mobile-menu-body"></div>
+	</div>
+
+	<nav class="site-nav">
+		<div class="container">
+			<div class="menu-bg-wrap">
+				<div class="site-navigation">
+					<div class="row g-0 align-items-center">
+						<div class="col-2">
+							<a href="index.html" class="logo m-0 float-start">Wiki<span class="text-primary"><Sup>TM</Sup></span></a>
+						</div>
+						<div class="col-8 text-center">
+							<form action="#" class="search-form d-inline-block d-lg-none">
+								<input type="text" class="form-control" placeholder="Search...">
+								<span class="bi-search"></span>
+							</form>
+
+							<ul class="js-clone-nav d-none d-lg-inline-block text-start site-menu mx-auto">
+								<li class="active"><a href="index.html">Home</a></li>
+								<li class="active"><a href="<?php echo URLROOT; ?>/AddWiki">Add a wiki</a></li>
+								<li class="has-children">
+									<a href="<?php echo URLROOT; ?>/home/getAllCategories">Categories <ion-icon name="chevron-down-outline"></ion-icon></a>
+									<ul class="dropdown">
+										<?php foreach ($data["categories"] as $category) { ?>
+											<li><a href="#"><?php echo $category->__get("name"); ?></a></li>
+										<?php
+										}  ?>
+									</ul>
+								</li>
+							</ul>
+						</div>
+						<div class="col-2 text-end">
+							<a href="#" class="burger ms-auto float-end site-menu-toggle js-menu-toggle d-inline-block d-lg-none light">
+								<span></span>
+							</a>
+							<form action="#" class="search-form d-none d-lg-inline-block">
+								<input type="text" class="form-control" placeholder="Search...">
+								<span class="bi-search"></span>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</nav>
