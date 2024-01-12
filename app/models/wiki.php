a26098af136715a0db276d8d082cc62dbb39e289
Aya <ayaelrhayour123@gmail.com>
2024-01-12 16:39:11 +0100

    modified wiki

--- app/models/wiki.php
+++ app/models/wiki.php
@@ -11,9 +11,16 @@ class Wiki{
   private $user_id;
   private $archived;
 
-  public function __construct()
+  public function __construct($id = null, $title = "",$content= "",$date ="" ,$image = "",$category_id ="" ,$user_id ="",$archived = null )
   {
-    
+    $this->id = $id;
+    $this->title = $title; 
+    $this->image = $image;
+    $this->content = $content;
+    $this->date = $date; 
+    $this->category_id = $category_id;
+    $this->user_id = $user_id;
+    $this->archived = $archived;
   }
    
   public function __set($property, $value){
