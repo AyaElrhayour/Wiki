ba1a17d70bc3d8d01410e8b706cf44f1e459e6a1
Aya <ayaelrhayour123@gmail.com>
2024-01-12 16:38:36 +0100

    modified wikiDao

--- app/dao/wikiDao.php
+++ app/dao/wikiDao.php
@@ -1,26 +1,82 @@
 <?php
 
 
-class WikiDao extends crudDao{
-
-public function __construct()
+class WikiDao extends crudDao
 {
-  parent::__construct();
-  $this->tablename= 'wikis';
-}
 
-public function getAllWikis(){
-  return $this->getAll();
-}
+  private $wikiEntity;
 
-public function insertWikis($data){
-  return $this->insert($data);
-}
+  public function __construct()
+  {
+    parent::__construct();
+    $this->tablename = 'wikis';
+    $this->wikiEntity = new Wiki();
+  }
 
-public function searchForWikis($column, $data){
-  return $this->search($column, $data);
-}
+  public function getAllWikis()
+  {
+    $this->db->query("SELECT * FROM wikis WHERE archived = 0");
+    $this->db->execute();
 
-}
+    $array = [];
+    $wikis = $this->db->resultSet();
+    foreach ($wikis as $wiki) {
+      $wikiEntity = new Wiki($wiki->id, $wiki->title, $wiki->content, $wiki->date, $wiki->image, $wiki->category_id, $wiki->user_id);
+      $array[] = $wikiEntity;
+    }
+    return $array;
+  }
 
-?>
+  public function getWikisByCategory($category_id)
+  {
+    $this->db->query("SELECT * FROM wikis WHERE archived = 0 AND category_id = :category_id");
+    $this->db->bind(':category_id', $category_id);
+    $this->db->execute();
+
+    $array = [];
+    $wikis = $this->db->resultSet();
+    foreach ($wikis as $wiki) {
+      $wikiEntity = new Wiki($wiki->id, $wiki->title, $wiki->content, $wiki->date, $wiki->image, $wiki->category_id, $wiki->user_id);
+      $array[] = $wikiEntity;
+    }
+    return $array;
+  }
+
+  public function getWikiById($id)
+  {
+    return $this->getById($id);
+  }
+
+  public function insertWikis($data)
+  {
+    $data = [
+      "id" =>$this->wikiEntity->__get("id"),
+      "title" =>$this->wikiEntity->__get("title"),
+      "content" =>$this->wikiEntity->__get("content"),
+      "date" =>$this->wikiEntity->__get("date"),
+      "image" =>$this->wikiEntity->__get("image"),
+      "category_id" =>$this->wikiEntity->__get("category_id"),
+      "user_id" =>$this->wikiEntity->__get("user_id"),
+    ];
+
+    $this->insert($data);
+
+  }
+
+  public function getLastWiki()
+  {
+    $this->db->query("SELECT * FROM wikis LIMIT 1");
+    $this->db->execute();
+    return $this->db->single();
+  }
+
+  public function archiveWiki($id)
+  {
+    $query = "UPDATE {$this->tablename} SET archived = 1 WHERE id = :id ";
+    $this->db->query($query);
+    $this->db->bind(':id', $id);
+    return $this->db->execute();
+  }
+
+  
+}
